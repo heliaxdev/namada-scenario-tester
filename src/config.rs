c0c66bdddc8fdc1f1bc7b5ef94d01250a48d5c7a
Gianmarco Fraccaroli <>
2023-12-18 15:18:53 +0100

    minors

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,5 @@
-#[derive(clap::ValueEnum, Clone, Debug, Copy)]
-pub enum CargoEnv {
-    Development,
-    Production,
-}
-
 #[derive(clap::Parser, Clone)]
 pub struct AppConfig {
-    #[clap(long, env, value_enum)]
-    pub cargo_env: CargoEnv,
-
     #[clap(long, env)]
     pub scenario: String,
 
