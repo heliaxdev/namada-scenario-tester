72e78e2caf419b83205a3a4a27546a9f3cb47c70
Gianmarco Fraccaroli <fraccaman@Gianmarcos-MBP.homenet.telecomitalia.it>
2024-04-05 14:56:19 +0200

    wip

--- src/gen/steps/init_default_proposal.rs
+++ src/gen/steps/init_default_proposal.rs
@@ -72,9 +72,9 @@ impl Step for InitDefaultProposal {
 
     fn total_post_hooks(&self) -> u64 {
         if self.author.eq(&self.tx_settings.gas_payer) {
-            1 // 2
+            2
         } else {
-            1 // 3
+            3
         }
     }
 
