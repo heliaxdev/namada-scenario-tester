a66ee52904ef5cd04724c0304ec6c9c950abce4d
Gianmarco Fraccaroli <fraccaman@Gianmarcos-MBP.homenet.telecomitalia.it>
2024-04-12 11:12:22 +0200

    minors

--- src/gen/constants.rs
+++ src/gen/constants.rs
@@ -1,8 +1,8 @@
 use namada_sdk::token::NATIVE_SCALE;
 
-pub const PROPOSAL_FUNDS: u64 = 500 * NATIVE_SCALE;
-pub const MIN_FEE: u64 = 25000;
-pub const DEFAULT_GAS_LIMIT: u64 = 25_000;
+pub const PROPOSAL_FUNDS: u64 = 5000 * NATIVE_SCALE;
+pub const MIN_FEE: u64 = 25_000;
+pub const DEFAULT_GAS_LIMIT: u64 = MIN_FEE;
 pub const DEFAULT_GAS_PRICE: f64 = 0.000001;
 pub const VALIDATOR_ZERO_STORAGE_KEY: &str = "validator-0-address";
 pub const BOND_VALIDATOR_STORAGE_KEY: &str = "validator-address";
