8c8ba11ea0d53b91b7954c298f666e0f066c5821
Gianmarco Fraccaroli <gianmarco.fraccaroli@gmail.com>
2024-08-08 11:07:06 +0200

    minors

--- src/tasks/mod.rs
+++ src/tasks/mod.rs
@@ -113,19 +113,19 @@ pub trait Task {
     }
 
     fn get_tx_errors(tx: &Tx, tx_response: &ProcessTxResponse) -> Option<String> {
-        let _cmt = tx.first_commitments().unwrap().to_owned();
-        let _inner_tx_hash = tx.header_hash();
+        let cmt = tx.first_commitments().unwrap().to_owned();
         let wrapper_hash = tx.wrapper_hash();
         match tx_response {
             ProcessTxResponse::Applied(result) => match &result.batch {
                 Some(batch) => {
-                    match batch.get_inner_tx_result(wrapper_hash.as_ref(), either::Right(&_cmt)) {
+                    match batch.get_inner_tx_result(wrapper_hash.as_ref(), either::Right(&cmt)) {
                         Some(Ok(res)) => {
                             let errors = res.vps_result.errors.clone();
                             let _status_flag = res.vps_result.status_flags;
                             let _rejected_vps = res.vps_result.rejected_vps.clone();
                             Some(serde_json::to_string(&errors).unwrap())
                         }
+                        Some(Err(e)) => Some(e.to_string()),
                         _ => None,
                     }
                 }
